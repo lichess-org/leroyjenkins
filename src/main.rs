c01b69c363dbf85e0b2e84d34139b6ffb56677b2
Lakin Wecker <lakin@wecker.ca>
2023-09-04 04:25:21 -0600

    Leeeeeeeeroy Jenkins

--- src/main.rs
+++ src/main.rs
@@ -17,68 +17,90 @@ use std::time::Instant;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// File that we'll follow
+    /// The ban log file that will be `tail -F`ed
     #[arg(short, long)]
-    ban_log: String,
+    bl_file: String,
 
-    /// The time we'll keep records around
-    #[arg(short, long)]
-    time_to_live: u64,
+    /// The time we'll keep the ban_log buckets around.
+    /// the user must avoid an nginx ban for this long before their
+    /// previous nginx bans are forgotten. Note, the first ban should probably
+    /// be long enough that this will expire during its duration
+    #[arg(long)]
+    bl_ttl: u64,
 
-    /// The number of times they show up before we ban them.
-    #[arg(short, long)]
-    instances_before_ban: u32,
+    /// The number of times they can show up in the ban log before hammer-time
+    #[arg(long)]
+    bl_threshold: u32,
 
-    /// the first ban time in seconds
+    /// Recidivists get banned for longer for their subsequent bans,
+    /// this reperesents the amount of time we'll keep this history around.
+    /// Everytime we :hammer-time: them, it will reset this countdown
+    /// the user must avoid an ipset ban for this long before their
+    /// previous ipset bans are forgotten.
+    #[arg(long)]
+    ipset_ban_ttl: u64,
+
+    /// (In seconds): The time of the first ban. Each subsequent ban will be increased
+    /// linearly by this amount (ban_count * base_time)
     #[arg(short, long)]
-    first_ban_time: u32,
+    ipset_base_time: u32,
 
     /// The name of the ipset for ipv6
     #[arg(short, long)]
-    ipv6_set_name: String,
+    ipset_ipv6_name: String,
 
     /// The name of the ipset for ipv4
     #[arg(short, long)]
-    ipv4_set_name: String,
+    ipset_ipv4_name: String,
 
-    /// The max size of the ipset
+    /// The max size of both ipsets.
     #[arg(short, long, default_value = "10000000")]
-    max_set_size: u32,
+    ipset_max_size: u32,
 
     /// The number of seconds to accumulate ban counts before reporting and resetting.
     #[arg(short, long, default_value = "600")]
-    ban_reporting_time_period: u128,
+    reporting_ban_time_period: u128,
 
     /// The number of seconds to accumulate ip counts before reporting and resetting.
     #[arg(short, long, default_value = "600")]
-    ip_reporting_time_period: u128,
+    reporting_ip_time_period: u128,
 }
 
 fn time_to_ban(args: &Args, ban_count: u32) -> u32 {
-    args.first_ban_time * ban_count
+    args.ipset_base_time * ban_count
+}
+
+fn log_and_ignore_err<T, E: std::fmt::Debug>(prefix: &'static str, res: Result<T, E>) {
+    if let Err(e) = res {
+        error!("{}: {:?}", prefix, e);
+    }
 }
 
 fn follow_banlog(args: &Args) -> io::Result<()> {
-    let mut ipv4 = Session::<HashIp>::new(args.ipv4_set_name.clone());
-    if let Err(e) = ipv4.create(|builder| {
-        builder
-            .with_ipv6(false)?
-            .with_max_elem(args.max_set_size)?
-            .build()
-    }) {
-        error!("Error creating ipv4 set {}: {:?}", &args.ipv4_set_name, e);
-    }
-    let mut ipv6 = Session::<HashIp>::new(args.ipv6_set_name.clone());
-    if let Err(e) = ipv6.create(|builder| {
-        builder
-            .with_ipv6(true)?
-            .with_max_elem(args.max_set_size)?
-            .build()
-    }) {
-        error!("Error creating ipv6 set {}: {:?}", &args.ipv4_set_name, e);
-    }
+    let mut ipv4 = Session::<HashIp>::new(args.ipset_ipv4_name.clone());
+    log_and_ignore_err(
+        "Error creating ipv4 set {}",
+        ipv4.create(|builder| {
+            builder
+                .with_ipv6(false)?
+                .with_timeout(args.ipset_base_time)?
+                .with_max_elem(args.ipset_max_size)?
+                .build()
+        }),
+    );
+    let mut ipv6 = Session::<HashIp>::new(args.ipset_ipv6_name.clone());
+    log_and_ignore_err(
+        "Error creating ipv6 set {}",
+        ipv6.create(|builder| {
+            builder
+                .with_ipv6(true)?
+                .with_timeout(args.ipset_base_time)?
+                .with_max_elem(args.ipset_max_size)?
+                .build()
+        }),
+    );
     let mut cmd = Command::new("tail")
-        .args(vec!["-F", &args.ban_log.clone()[..]])
+        .args(vec!["-F", &args.bl_file.clone()[..]])
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
@@ -86,12 +108,18 @@ fn follow_banlog(args: &Args) -> io::Result<()> {
     {
         let stdout = cmd.stdout.as_mut().unwrap();
         let mut stdout_reader = BufReader::new(stdout);
-        info!("Following {:?}", args.ban_log);
+        info!("Following {:?}", args.bl_file);
 
-        let mut cache: Cache<_, _, BuildHasherDefault<FxHasher>> = Cache::builder()
+        let mut ban_log_count_cache: Cache<_, _, BuildHasherDefault<FxHasher>> = Cache::builder()
             // Max 10,000 elements
             .max_capacity(500_000) // TODO: I made this number up?
-            .time_to_live(Duration::from_secs(args.time_to_live))
+            .time_to_live(Duration::from_secs(args.bl_ttl))
+            .build_with_hasher(Default::default());
+
+        let mut ipset_ban_count_cache: Cache<_, _, BuildHasherDefault<FxHasher>> = Cache::builder()
+            // Max 10,000 elements
+            .max_capacity(500_000) // TODO: I made this number up?
+            .time_to_live(Duration::from_secs(args.ipset_ban_ttl))
             .build_with_hasher(Default::default());
 
         let mut line = String::new();
@@ -102,18 +130,29 @@ fn follow_banlog(args: &Args) -> io::Result<()> {
         let mut ip_count_start = Instant::now();
         while stdout_reader.read_line(&mut line)? != 0 {
             ip_count += 1;
-            let count: u32 = *cache.get(&line).unwrap_or(&0) + 1;
-            cache.insert(line.clone(), count);
-            if count + 1 >= args.instances_before_ban {
+            let ban_log_count: u32 = *ban_log_count_cache.get(&line).unwrap_or(&0) + 1;
+            ban_log_count_cache.insert(line.clone(), ban_log_count);
+            if ban_log_count + 1 >= args.bl_threshold {
+                let ipset_ban_count: u32 = *ipset_ban_count_cache.get(&line).unwrap_or(&0) + 1;
                 match line[..line.len() - 1].parse::<IpAddr>() {
                     Ok(ip) => {
                         ban_count += 1;
                         _ = match ip {
-                            IpAddr::V4(_) => ipv4.add(ip, Some(time_to_ban(args, count))),
-                            IpAddr::V6(_) => ipv6.add(ip, Some(time_to_ban(args, count))),
+                            IpAddr::V4(_) => {
+                                log_and_ignore_err(
+                                    "Unable to add to ipv4 set",
+                                    ipv4.add(ip, Some(time_to_ban(args, ipset_ban_count))),
+                                );
+                            }
+                            IpAddr::V6(_) => {
+                                log_and_ignore_err(
+                                    "Unable to add to ipv6 set",
+                                    ipv6.add(ip, Some(time_to_ban(args, ipset_ban_count))),
+                                );
+                            }
                         };
                         if ban_count_start.elapsed().as_millis()
-                            > args.ban_reporting_time_period * 1000
+                            > args.reporting_ban_time_period * 1000
                         {
                             info!(
                                 "Banned {} ips in the past {:?}",
@@ -127,7 +166,7 @@ fn follow_banlog(args: &Args) -> io::Result<()> {
                     Err(e) => error!("Error parsing ip line: {:?}", e),
                 }
             }
-            if ip_count_start.elapsed().as_millis() > args.ip_reporting_time_period * 1000 {
+            if ip_count_start.elapsed().as_millis() > args.reporting_ip_time_period * 1000 {
                 info!("Seen {} ips since {:?}", ip_count, ip_count_start.elapsed());
                 ip_count = 0;
                 ip_count_start = Instant::now();
